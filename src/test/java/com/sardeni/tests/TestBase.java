6a88137b4cccce0be7fe8e2b1520a4edf92c6641
Sardeni <nordisnsk@gmail.com>
2023-02-24 23:39:28 +0400

    initial commit

---
+++ src/test/java/com/sardeni/tests/TestBase.java
@@ -0,0 +1,44 @@
+package com.sardeni.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.sardeni.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browser = System.getProperty("browserName", "chrome");
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://hh.ru";
+        Configuration.remote = System.getProperty("remoteURL");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListenerAndOpenPage() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("/vacancy/75860919");
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
---
+++ src/test/java/com/sardeni/tests/Tests.java
@@ -0,0 +1,20 @@
+package com.sardeni.tests;
+
+import com.codeborne.selenide.Condition;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class Tests extends TestBase{
+
+
+}
